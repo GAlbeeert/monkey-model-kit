a029f145cdb54180056abf9dd2e78cae2167f7c3
Albert <albert.cosoreanu@epitech.eu>
2022-01-04 21:43:46 +0100

    [:bulb:] {MMK.c} : Built file stocking the main function

---
+++ src/MMK.c
@@ -0,0 +1,18 @@
+/*
+** COALAS PROJECT, 2021
+** MAIN
+** File description:
+** File containing the main MMK's functions
+*/
+
+#include "MMK.h"
+
+int main(int argc, char **argv)
+{
+    int len = strlen(argv[1]);
+    char *buff = malloc(sizeof(char) * len + 1);
+
+    file_opener(argv[1]);
+    free(buff);
+    return 0;
+}
