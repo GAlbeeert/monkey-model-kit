01ca5d0903bc345b09a23b97623bea0dceb7d70c
Albert <albert.cosoreanu@epitech.eu>
2022-01-04 22:42:50 +0100

    [:sparkles:] {MMK.c} : When no user is given, the file creates an "Unnanmed.txt" file, else it uses the user's name

--- src/MMK.c
+++ src/MMK.c
@@ -12,7 +12,11 @@ int main(int argc, char **argv)
     int len = strlen(argv[1]);
     char *buff = malloc(sizeof(char) * len + 1);
 
-    file_opener(argv[1]);
+
+    if (argc == 1)
+        file_opener("Unnamed");
+    else if (argc == 2)
+        file_opener(argv[1]);
     free(buff);
     return 0;
 }
