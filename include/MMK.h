f36abba1109201fcf14fee4be3657afa76407d53
Albert <albert.cosoreanu@epitech.eu>
2022-01-04 21:44:32 +0100

    [:bulb:] {MMK.h} : Added header file

---
+++ include/MMK.h
@@ -0,0 +1,20 @@
+/*
+** COALAS PROJECT, 2021
+** MAINH
+** File description:
+** Header file for the MMK project
+*/
+
+#ifndef MMK_MMK_H
+#define MMK_MMK_H
+
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+//File opening or creating functions
+int file_opener(char *user);
+
+#endif //MMK_MMK_H
